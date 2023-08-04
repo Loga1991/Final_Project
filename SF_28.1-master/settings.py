9fcaaf62074e32d9deb59a79227c8da707046165
Loga1991 <125743391+Loga1991@users.noreply.github.com>
2023-08-05 00:08:47 +0300

    Add files via upload

---
+++ SF_28.1-master/settings.py
@@ -0,0 +1,18 @@
+class Settings:
+
+
+    valid_email = 'lozhkinagv@gmail.com'
+    valid_password = '2wsx$RFV'
+    valid_email_reg = 'ok*********@gmail.com'
+    region_select = 'Москва г'
+    invalid_password = '1234ллывр'
+    menu_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Катя'
+    last_name = 'Кошкина'
+    cyrillic = 'а'
+    empty = ''
+    numbers = 333
+    latin = 'invalid'
+    chinese = '漢字漢字漢字漢字漢字'
+    special_character = '!"№;%:?*()!"'
+
